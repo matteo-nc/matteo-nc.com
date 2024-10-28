5d50d4dcb88e85bd1fc1064bddac8dd76f169055
Matteo NADLER CAMPOURCY <matteo.nadler.campou@gmail.com>
2024-10-28 15:20:41 +0100

    added +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<div class="w-dvh h-dvh bg-white dark:bg-black">
+    <div class="flex h-full justify-center items-center">
+        <h1 class="md:text-7xl sm:text-6xl text-5xl text-black bg-transparent font-jetBrainsMono dark:text-white dark:text-shadow dark:shadow-white">matteo-nc.com</h1>
+    </div>
+    <footer>
+        <div class="flex justify-center items-center h-16 bg-black dark:bg-white">
+            <p class="text-black dark:text-gray-200">© 2024 Mattéo NADLER CAMPOURCY</p>
+        </div>
+    </footer>
+</div>
+
+<style>
+
+</style>
