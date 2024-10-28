b5e14998817e4c302521fddf9c60b6a00ce5c3e3
Matteo NADLER CAMPOURCY <matteo.nadler.campou@gmail.com>
2024-10-28 16:11:31 +0100

    added copyright in bottom of page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,13 @@
 
 </script>
 
-<div class="w-dvh h-dvh bg-white dark:bg-black">
-    <div class="flex h-full justify-center items-center">
+<div class="flex flex-col w-dvh h-dvh bg-white dark:bg-black">
+    <div class="flex flex-grow justify-center items-center">
         <h1 class="md:text-7xl sm:text-6xl text-5xl text-black bg-transparent font-jetBrainsMono dark:text-white dark:text-shadow dark:shadow-white">matteo-nc.com</h1>
     </div>
-    <footer>
-        <div class="flex justify-center items-center h-16 bg-black dark:bg-white">
-            <p class="text-black dark:text-gray-200">© 2024 Mattéo NADLER CAMPOURCY</p>
-        </div>
-    </footer>
+    <div class="flex justify-center items-end">
+        <p class="text-xs text-gray-800 dark:text-gray-200">&copy; 2024 Mattéo NADLER CAMPOURCY</p>
+    </div>
 </div>
 
 <style>
