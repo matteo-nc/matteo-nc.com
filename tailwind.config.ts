2c1087c183dec47c0745f9358a673a7639d19624
Matteo NADLER CAMPOURCY <matteo.nadler.campou@gmail.com>
2025-02-17 12:41:24 +0100

    Fixed deno deploy + node adapter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,30 @@
-import type { Config } from 'tailwindcss';
-import plugin from 'tailwindcss/plugin';
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
     extend: {
       textShadow: {
-        DEFAULT: '0 0 8px var(--tw-shadow-color)'
+        DEFAULT: "0 0 8px var(--tw-shadow-color)",
       },
     },
     fontFamily: {
-      'jetBrainsMono': ["JetBrains Mono", 'monospace'],
-    }
+      "jetBrainsMono": ["JetBrains Mono", "monospace"],
+    },
   },
 
   plugins: [
     plugin(function ({ matchUtilities, theme }) {
       matchUtilities(
-          {
-            'text-shadow': (value) => ({
-              textShadow: value,
-            }),
-          },
-          { values: theme('textShadow') }
-      )
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") },
+      );
     }),
   ],
 } as Config;
